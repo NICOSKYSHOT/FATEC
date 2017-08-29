46a348a3b24d886c0fb47eb094a9a424953422cb
Vitor Lamounier <vitflamo@hotmail.com>
2017-08-29 19:08:45 -0300

    08-29 1
    Robo

--- Java/08-22 Robo/src/testarobo/TestaRobo.java
+++ Java/08-22 Robo/src/testarobo/TestaRobo.java
@@ -12,7 +12,6 @@ public class TestaRobo {
 		double n1=0, n2=0;
 	
 		while (op!=7){
-			r1.setBateria(r1.getBateria());
 			op = Integer.parseInt(JOptionPane.showInputDialog(null, "MENU" + 
 					"\n1 - Criar Robô" + 
 					"\n2 - Operação Soma" + 
@@ -24,11 +23,40 @@ public class TestaRobo {
 					"\n\nNome do Robo: " + r1.getNome() +
 					"\nData de Fabricação " + r1.getDtfab(), "MENU ROBO - BATERIA = " + r1.getBateria(),1)); 
 		
-				r1.insercao(op);
-			
-			
-		}
+				if(op==1) {
+					
+					r1.setNome(JOptionPane.showInputDialog(null, "Insira o nome para o Robo:"));
+					r1.setDtfab((JOptionPane.showInputDialog(null, "Insira a data de fabricação do Robo:")));
+					r1.setBateria(Integer.parseInt(JOptionPane.showInputDialog(null, "Insira a bateria do robo:")));
+					
+				}else if (op>1 && op<6 && r1.getBateria()>0) {
+
+					n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o primeiro valor:"));
+					n2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o segundo valor:"));
+				
+					if (op==2) {
+						JOptionPane.showMessageDialog(null, "O resultado da soma é: " + r1.Soma(n1,n2));
+					}else if (op==3) {
+						JOptionPane.showMessageDialog(null, "O resultado da subtração é: " + r1.Sub(n1,n2));
+					}else if (op==4) {
+						JOptionPane.showMessageDialog(null, "O resultado da multiplicação é: " + r1.Multi(n1,n2));
+					}else if (op==5) {
+						JOptionPane.showMessageDialog(null, "O resultado da divisão é: " + r1.Div(n1,n2));
+					}else {
+						JOptionPane.showMessageDialog(null, "Opção Inválida", "ERRO", 0);
+					}
+					
+					r1.setBateria(r1.getBateria()-1);
+					
+				}else if(op==6) {
+					JOptionPane.showMessageDialog(null, "Bateria Carregada +1", "Atenção", 2);
+					r1.setBateria(r1.getBateria()+1);
+				}else if(op==7){
+					System.exit(0);
+				}else {
+					JOptionPane.showMessageDialog(null, "Opção Inválida\nVerifique a Bateria do Robô", "ERRO", 0);
+				}
 		
 	}
-
+	}
 }
